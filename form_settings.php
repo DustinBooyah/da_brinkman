dc83375b8b68d03feebb3d65456866d519360755
Dustin Aguilar <dustin@dustinaguilar.com>
2015-04-22 02:34:29 -0400

    Committing additions to form
    Plugging in existing form I created.

---
+++ form_settings.php
@@ -0,0 +1,7 @@
+<?php
+
+$email_to = "eightball007@hotmail.com"; // your email address
+$email_subject = "Estimate request from website"; // email subject line
+$thankyou = "thanks.html"; // thank you page
+
+?>
