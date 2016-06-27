3c2a0e8960ead4128010b5287cfa0154c8754205
DustinLaptop <dustin@dustinaguilar.com>
2016-06-26 22:57:47 -0400

    attempting to fix header issue
    Attempting to fix header issue

--- mailer.php
+++ mailer.php
@@ -11,25 +11,23 @@
 
 ###################### Set up the following variables ###################### 
 # 
-$to = "kim.brinkman@att.net"; #set address to send form to 
-$subject = "Survey Estimate Request"; #set the subject line 
-$forward = 1; # redirect? 1 : yes || 0 : no 
-$location = "thanks.html"; #set page to redirect to, if 1 is above \
-$name = $_POST['Client_Name'];
-$email = $_POST['client_email'];
-$headers .= "From: ".$name." <".$email.">";
+$to = "kim.brinkman@att.net"; //set address to send form to
+$subject = "Survey Estimate Request"; //set the subject line
+$from = $_POST['client_email']; // this is the sender's Email address
+$name = $_POST['client_name']; 
+$forward = 1; // redirect? 1 : yes || 0 : no 
+$location = "thanks.html"; //set page to redirect to, if 1 is above
+$headers .= "From: ".$name." <".$from.">";
 # 
 ##################### No need to edit below this line ###################### 
 
 ## set up the time ## 
 
 $date = $_POST['begin_date']; 
-$time = date ("h:i A"); 
+$time = date ("h:i A");
 
 ## mail the message ## 
-
-$msg = "On $date at $time, the following Survey Estimate Request was submitted via the Brinkman Surveying website.\n\n"; 
-
+$msg = "Hi,\n\n A Survey Estimate Request was made at $time via the Brinkman Surveying website. \n\n";
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	foreach ($_POST as $key => $value) { 
 		$msg .= ucfirst ($key) ." : ". $value . "\n"; 
@@ -48,5 +46,4 @@ if ($forward == 1) {
 else { 
     echo "Thank you!. We'll get back to you as soon as possible."; 
 } 
-
 ?>
